2b71dae27155f5ec508a310310565e6238971d06
DESKTOP-7N7IVO4\user <u0151057@nkfust.edu.tw>
2022-02-02 16:04:55 +0800

    add google-drive-downloader weight add torchvision require package

---
+++ download.py
@@ -0,0 +1,44 @@
+from google_drive_downloader import GoogleDriveDownloader as gdd
+
+gdd.download_file_from_google_drive(file_id='1KKMZJhcHI_YlHFR7prIMjvXqlBcqA895',
+                                    dest_path='./model_data/deeplab_mobilenetv2.pth',
+                                    unzip=False,
+                                    showsize=True,
+                                    overwrite=True)
+
+gdd.download_file_from_google_drive(file_id='16yqVFKT9ARZGJxhUlAJsi3ccWE6xQAGN',
+                                    dest_path='./model_data/deeplab_xception.pth',
+                                    unzip=False,
+                                    showsize=True,
+                                    overwrite=True)        
+
+
+gdd.download_file_from_google_drive(file_id='1Rn7u7lRzL1Uc-CTT2gR5JSnPtkwvgY4t',
+                                    dest_path='./model_data/mobilenet_v2.pth.tar',
+                                    unzip=False,
+                                    showsize=True,
+                                    overwrite=True)      
+
+gdd.download_file_from_google_drive(file_id='1N9vqYmwiFS_WVhFf2BhsnZmgW0lqUMDK',
+                                    dest_path='./model_data/pspnet_mobilenetv2.pth',
+                                    unzip=False,
+                                    showsize=True,
+                                    overwrite=True)  
+
+gdd.download_file_from_google_drive(file_id='1S5y3lujuNRcD9TrmUqSiPOySctniyvvR',
+                                    dest_path='./model_data/pspnet_resnet50.pth',
+                                    unzip=False,
+                                    showsize=True,
+                                    overwrite=True)       
+
+gdd.download_file_from_google_drive(file_id='1jVQvcTOOa3fw58OWHfqg-1QftOHSDLhh',
+                                    dest_path='./model_data/unet_resnet_voc.pth',
+                                    unzip=False,
+                                    showsize=True,
+                                    overwrite=True)    
+
+gdd.download_file_from_google_drive(file_id='14eGgwmnJTXmlvD6RTRIURbzyOYbleLl8',
+                                    dest_path='./model_data/unet_vgg_voc.pth',
+                                    unzip=False,
+                                    showsize=True,
+                                    overwrite=True)                                                                                                 
