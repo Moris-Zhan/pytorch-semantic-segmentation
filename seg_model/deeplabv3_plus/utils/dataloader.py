d10331c8e96c10d8d628b2a64cc8192cf511b3e7
Leyan <afly.bsky@gmail.com>
2023-01-20 04:28:59 +0800

    Add files via upload

--- seg_model/deeplabv3_plus/utils/dataloader.py
+++ seg_model/deeplabv3_plus/utils/dataloader.py
@@ -62,38 +62,6 @@ class DeeplabDataset(Dataset):
 
         return jpg, png, seg_labels
     
-    
-    def __getitem2__(self, index):
-        annotation_line = self.annotation_lines[index]
-        name            = annotation_line.split()[0]
-
-        #-------------------------------#
-        #   从文件中读取图像
-        #-------------------------------#
-        # jpg         = Image.open(os.path.join(os.path.join(self.dataset_path, "VOC2007/JPEGImages"), name + ".jpg"))
-        # png         = Image.open(os.path.join(os.path.join(self.dataset_path, "VOC2007/SegmentationClass"), name + ".png"))
-        jpg         = Image.open(annotation_line.split()[0] + ".jpg")
-        png         = Image.open(annotation_line.split()[1] + ".png")
-        #-------------------------------#
-        #   数据增强
-        #-------------------------------#
-        jpg, png    = self.get_random_data(jpg, png, self.input_shape, random = self.train)
-
-        jpg         = np.transpose(preprocess_input(np.array(jpg, np.float64)), [2,0,1])
-        png         = np.array(png)
-        png[png >= self.num_classes] = self.num_classes
-        #-------------------------------------------------------#
-        #   转化成one_hot的形式
-        #   在这里需要+1是因为voc数据集有些标签具有白边部分
-        #   我们需要将白边部分进行忽略，+1的目的是方便忽略。
-        #-------------------------------------------------------#
-        seg_labels  = np.eye(self.num_classes + 1)[png.reshape([-1])]
-        seg_labels  = seg_labels.reshape((int(self.input_shape[0]), int(self.input_shape[1]), self.num_classes + 1))
-
-        return jpg, png, seg_labels
-
-
-
     def rand(self, a=0, b=1):
         return np.random.rand() * (b - a) + a
 
